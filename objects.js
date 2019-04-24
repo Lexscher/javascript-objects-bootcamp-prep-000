1eae116f55e68b564d40abd2f3010283bff8d265
Alexander Schelchere <ascellie@gmail.com>
2019-04-24 20:17:23 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -33,6 +33,12 @@ function updatePlaylist(myPlaylist, artistName, songTitle) {
 }
 
 function removeFromPlaylist(myPlaylist, artistName) {
-  delete myPlaylist.artistName;
-  return myPlaylist;
+  console.log(myPlaylist.artistName);
+  delete myPlaylist.artistName
+  if (delete myPlaylist.artistName) {
+    console.log(myPlaylist);
+    return myPlaylist;
+  } else {
+    console.log("unsuccessful.");
+  }
 }
